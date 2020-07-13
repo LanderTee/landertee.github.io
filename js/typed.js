5807a1204fa0424d4e88396fd2679edb9fc8681a
landertee <landertee@qq.com>
2020-07-13 16:59:19 +0800

    comment

--- js/typed.js
+++ js/typed.js
@@ -396,7 +396,7 @@
     };
 
     $.fn.typed.defaults = {
-        strings: ["My name is Lander", "You know what you should do?", "Use your own!", "Have a great day!"],
+        strings: ["This is just an example", "You know what you should do?", "Use your own!", "Have a great day!"],
         stringsElement: null,
         // typing speed
         typeSpeed: 0,
